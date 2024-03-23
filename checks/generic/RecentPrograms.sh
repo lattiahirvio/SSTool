29e9fc939465c6b0b0746f1e9ce2c1202ad58fdf
iravii <lattiahirvio@gmail.com>
2024-03-24 01:59:39 +0200

    updated stuff ig?

--- checks/generic/RecentPrograms.sh
+++ checks/generic/RecentPrograms.sh
@@ -2,5 +2,5 @@
 
 # Execute the command and append output to results.txt
 ps k-etime h -eo etimes,command | while read etime comm; do
-    [ "$etime" -lt 50000 ] && echo -e "$etime\t$comm" >> output/results.txt
+    [ "$etime" -lt 10000 ] && echo -e "$etime\t$comm" >> output/results.txt
 done
