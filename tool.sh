e14e4b5895b7b02a3f9a17950c32e968749b0a8e
lattiahirvio <69205821+lattiahirvio@users.noreply.github.com>
2025-12-30 05:34:11 +0200

    Add copyright and license details to tool.sh
    Added copyright and licensing information to the script.

--- tool.sh
+++ tool.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2025 lattiahirvio
+#
+# This file is part of SSTool.
+#
+# SSTool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SSTool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SSTool.  If not, see <https://www.gnu.org/licenses/>.
+#
 # Fail safely (oops)
 set -Eeuo pipefail
 IFS=$'\n\t'
